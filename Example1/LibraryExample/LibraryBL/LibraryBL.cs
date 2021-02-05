f3d41c261dd5f7e3356883ab6223b4d036bbecd9
v.ponomarenko <v.ponomarenko@traderevolution.com>
2021-02-05 21:55:00 +0200

    5

---
+++ Example1/LibraryExample/LibraryBL/LibraryBL.cs
@@ -0,0 +1,22 @@
+﻿using CommonEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBusinessLayer
+{
+    public class LibraryBL : ILibraryBL
+    {
+        ILibraryData _dataLayer;
+        public LibraryBL(ILibraryData data)
+        {
+            _dataLayer = data;
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Example1/LibraryExample/LibraryDataLayer/LibraryData.cs
@@ -0,0 +1,17 @@
+﻿using CommonEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDataLayer
+{
+    public class LibraryData : ILibraryData
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
